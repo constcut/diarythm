25413b3072368789fa7d5da7fb15a958963f2cd8
Konstantin Kutsenko <ssidein@gmail.com>
2022-03-31 16:21:53 +0300

    Basic json errors and start of parsing

--- app/DiaryCardEngine.cpp
+++ app/DiaryCardEngine.cpp
@@ -1,9 +1,30 @@
 #include "DiaryCardEngine.hpp"
 
 #include <QJsonDocument>
-
+#include <QJsonArray>
+#include <QJsonObject>
+#include <QDebug>
 
 void DiaryCardEngine::parseJSON(const QString& json)
 {
+    auto doc = QJsonDocument::fromJson(json.toUtf8());
 
+    if (doc.isObject() == false) {
+        qDebug() << "Failed to parse: JSON root is not an object.";
+        return;
+    }
+
+    auto rootObject = doc.object();
+
+    if (rootObject.contains("groups") == false) {
+        qDebug() << "Failed to prase: JSON root doesn't contain groups field.";
+        return;
+    }
+
+    if (rootObject["groups"].isArray() == false) {
+        qDebug() << "Failed to parse: JSON groups field isn't array";
+        return;
+    }
+
+    auto groups = rootObject["groups"];
 }
